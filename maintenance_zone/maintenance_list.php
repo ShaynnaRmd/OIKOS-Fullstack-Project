4e48d3be672304eeb27be5a7ac5692273e20e855
shay <shaynna.raymond@gmail.com>
2023-06-12 10:32:17 +0200

    adding current month function

--- maintenance_zone/maintenance_list.php
+++ maintenance_zone/maintenance_list.php
@@ -24,25 +24,26 @@ $role_result = $role_requete->fetch(PDO::FETCH_ASSOC);
 if ($role_result && $role_result['maintenance_role'] == 1) {
     $maintenanceRole = $role_result['maintenance_role'];
     // Si le rôle maintenance_role est ok -> proceed le reste du code : */
+    $currentMonth = date('Y-m');
+
     $maintenance_requete = $website_pdo->prepare('
-    SELECT DISTINCT m.id, m.status, m.title, m.schedule_date, m.housing_id, hi.image, h.title AS housing_title
-    FROM maintenance m
-    JOIN housing_image hi ON m.housing_id = hi.housing_id  
-    JOIN housing h ON m.housing_id = h.id
-');
-$maintenance_requete->execute();
-$maintenance_result = $maintenance_requete->fetchAll(PDO::FETCH_ASSOC);
+        SELECT DISTINCT m.id, m.status, m.title, m.schedule_date, m.housing_id, hi.image, h.title AS housing_title
+        FROM maintenance m
+        JOIN housing_image hi ON m.housing_id = hi.housing_id  
+        JOIN housing h ON m.housing_id = h.id
+        WHERE DATE_FORMAT(m.schedule_date, "%Y-%m") = :currentMonth
+    ');
+    $maintenance_requete->bindParam(':currentMonth', $currentMonth, PDO::PARAM_STR);
+    $maintenance_requete->execute();
+    $maintenance_result = $maintenance_requete->fetchAll(PDO::FETCH_ASSOC);
 
-$housing_id = array();
-for ($i = 0; $i < count($maintenance_result); $i++) {
-    array_push($housing_id, $maintenance_result[$i]['housing_id']);
-}
+    $housing_id = array();
+    for ($i = 0; $i < count($maintenance_result); $i++) {
+        array_push($housing_id, $maintenance_result[$i]['housing_id']);
+    }
 
-$title = "Tâches à venir: ";
-
-
-
-
+    $title = "Tâches à venir pour le mois en cours: ";
+    
 /*// L'utilisateur n'a pas le role neccessaire -> le rediriger vers l'acceuil ou qqch comme ça :
 }else {
     echo "Vous n'avez pas les droits pour continuer.";
